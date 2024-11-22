841e8b06e9f85c98f3183a7cc3c7599b48f343b1
Rishabh Bijalwan <rishabhbijalwan19@gmail.com>
2024-11-22 19:57:02 +0530

    changed

--- client/src/App.js
+++ client/src/App.js
@@ -7,24 +7,35 @@ import Home from './components/Home';
 import Profile from './components/Profile';
 import CreatePost from './components/CreatePost';
 import UserProfile from './components/UserProfile';
-import {ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      localStorage.clear();
+    };
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []); // Empty dependency array ensures this runs once on mount
+
   return (
     <>
-    <Router>
-      <ToastContainer/>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/createpost" element={<CreatePost/>} />
-        <Route path="//userprofile/:userId" element={<UserProfile/>} />
-      </Routes>
-    </Router>
+      <Router>
+        <ToastContainer />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/createpost" element={<CreatePost />} />
+          <Route path="/userprofile/:userId" element={<UserProfile />} />
+        </Routes>
+      </Router>
     </>
   );
 }
